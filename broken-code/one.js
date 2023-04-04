889965e498de452e8f470d42ad08376d442fee31
Ed <josuegomo@hotmail.com>
2023-04-04 00:43:42 -0500

    🚧 Add Exercise 1 incomplete

---
+++ broken-code/one.js
@@ -0,0 +1,13 @@
+// Ejercicio #1
+
+// Hay una aplicación que muestra información pública de un tweet pero ésta necesita la id del tweet.
+// Se conoce que la url de un tweet contiene dicha id numérica al final de la url.
+// Ten en cuenta que el usuario te va a proporcionar url distintas, cómo también te puede proporcionar una url que no sea
+// de twitter, mucho menos de un tweet!
+// Crea una función que verifique que la url sea de twitter(es decir que sea la url de un tweet), recuperar y devolver la Id del tweet
+
+const tweetUrl = 'https://twitter.com/kansaita_/status/1641180205623525387'
+const tweetShareUrl= 'https://twitter.com/folklorebot/status/1641179258839248896?s=20'
+const noTwitterUrl = 'https://github.com/users/EDUJOS/projects/2/views/1?layout=board'
+
+// Escribe la función aquí
